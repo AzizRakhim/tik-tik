c636ee7e180605c31d5bddf1d9909addfcc0bf8a
Aziz Rakhim <azizbekrahimjonov80@gmail.com>
2023-06-13 23:03:17 +0500

    refactor: change footer creator text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -20,7 +20,7 @@ const Footer = () => {
       <List items={footerList1} mt={false} />
       <List items={footerList2} mt />
       <List items={footerList3} mt />
-      <p className="text-gray-400 text-sm mt-5">2023 Aziz Rakhm TikTik</p>
+      <p className="text-gray-400 text-sm mt-5">2023 Aziz Rakhim TikTik</p>
     </div>
   );
 };
