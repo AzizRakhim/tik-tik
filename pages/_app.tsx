5792032f5bc2033dbae00782d825b1ed7153a13c
Aziz Rakhim <azizbekrahimjonov80@gmail.com>
2023-06-13 16:09:41 +0500

    added head tag with title and meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import type { AppProps } from "next/app";
 import { useState, useEffect } from "react";
 import { GoogleOAuthProvider } from "@react-oauth/google";
@@ -21,6 +22,16 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}
     >
       <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh]">
+        <Head>
+          <title>Tik Tik</title>
+          <meta property="og:image" content="https://i.imgur.com/xnX65IH.png" />
+          <meta
+            property="og:image:secure_url"
+            content="https://i.imgur.com/xnX65IH.png"
+          />
+          <meta property="og:image:alt" content="Tik Tik" />
+        </Head>
+
         <Navbar />
         <div className="flex gap-6 md:gap-20">
           <div className="h-[92vh] overflow-hidden hover:overflow-y-auto">
